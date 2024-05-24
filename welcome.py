64db7cba7e10b078f8d188d0d5a28b15eecbd049
IamClodate <clodatemnisi@gmail.com>
2024-05-24 21:38:38 +0200

    h

---
+++ welcome.py
@@ -0,0 +1,13 @@
+
+# the menu 
+
+def main_menu():
+    print("Welcome to the Main Menu:")
+    print("1. Procurement")
+    print("2. Finance")
+    print("3. Water Monitoring")
+    print("4. Environmental Protection")
+    print("5. Logistics")
+
+if __name__ == "__main__":
+    main_menu()
